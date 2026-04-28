ee3ffd6771903caa0ad96e42c9c56d0bb384f05f
MagHawk <mag.hoegh@gmail.com>
2026-04-28 16:16:22 +0200

    Fixed path system for dependencies, added system for reading file to get f2aim and f2Idx data as well as new script to generate these. Will later be replaced by frequency distribution script

---
+++ generatef2aimDummy.m
@@ -0,0 +1,4 @@
+% This script should be replaced by frequency distribution algorithm
+f2aim = [500:500:1500 2000:1000:10000].';
+f2Idx = [2 3 4 6 7 8 11 12; 1 5 9 10 zeros(1,4)];
+save("dpFrequencies.mat", "f2aim", "f2Idx");
