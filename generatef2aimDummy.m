bf805fcd41cafa59b69d7ef75c09ad50c06c7dea
MagHawk <mag.hoegh@gmail.com>
2026-05-05 15:01:46 +0200

    changes to MFDP script now allow for multiple frequencies to be played and recorded. Levels still need adjusting

--- generatef2aimDummy.m
+++ generatef2aimDummy.m
@@ -1,4 +1,5 @@
 % This script should be replaced by frequency distribution algorithm
 f2aim = [500:500:1500 2000:1000:10000].';
-f2Idx = [2 3 4 6 7 8 11 12; 1 5 9 10 zeros(1,4)];
+% f2Idx = [2 3 4 6 7 8 11 12; 1 5 9 10 zeros(1,4)];
+f2Idx = [2 4 6 10; 3 5 9 0; 7 12 0 0];
 save("dpFrequencies.mat", "f2aim", "f2Idx");
