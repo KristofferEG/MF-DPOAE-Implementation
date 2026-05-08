44386511e58f0dd13ca0756655bd47ae8ca5e15c
MagHawk <mag.hoegh@gmail.com>
2026-05-08 10:44:45 +0200

    Added filtering of noise indices to avoid misrepresentative SNRs. Also changed f2AimDummy to not interfere with f2Aim file scheme

--- generatef2aimDummy.m
+++ generatef2aimDummy.m
@@ -4,4 +4,4 @@ f2aim = [500:500:1500 2000:1000:10000].';
 % f2Idx = [2 4 6 10; 3 5 8 0; 7 12 0 0];
 % f2Idx = [2 4 6; 3 5 8; 1 7 12];
 f2Idx = [2 4 6; 3 5 8];
-save("dpFrequencies.mat", "f2aim", "f2Idx");
+save("dpFrequenciesDummy.mat", "f2aim", "f2Idx");
