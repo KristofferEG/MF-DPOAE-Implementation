f9a48e6092c5db6e53fef172ba568cf7839ad5df
MagHawk <mag.hoegh@gmail.com>
2026-05-13 10:47:49 +0200

    Start of work on fixing levels for frequencies so calibration can be done individually

--- generateStimulus.m
+++ generateStimulus.m
@@ -1,7 +1,14 @@
-function [stimulusOut] = generateStimulus(nFreq, freq, t)
+function [stimulusOut] = generateStimulus(nFreq, freq, t, levelPerTone)
     stimulus = [0 0];
-    for i = nFreq
-        stimulus = stimulus + [cos(2*pi*freq(i,1)*t) cos(2*pi*freq(i,2)*t)];
+    if nargin <= 3
+        for i = nFreq
+            stimulus = stimulus + [cos(2*pi*freq(i,1)*t) cos(2*pi*freq(i,2)*t)];
+        end
+    else
+        levels = levelPerTone.*sqrt(2);
+        for i = nFreq
+            stimulus = stimulus + [cos(2*pi*freq(i,1)*t).*levels(i, 1) cos(2*pi*freq(i,2)*t).*levels(i, 2)];
+        end
     end
     
     stimulusOut = stimulus/i;
